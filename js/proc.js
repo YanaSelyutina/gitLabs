f25f269c62f1d08b51b5eb8a0b4c56bff72b8e0d
YanaSelyutina <selyutina1998@gmail.com>
2018-05-31 19:18:09 +0300

    lab1

---
+++ js/proc.js
@@ -0,0 +1,24 @@
+var OperationEnum = {
+	NONE: 0,
+	DIV: 1,
+	MUL: 2,
+	SUB: 3,
+	ADD: 4,
+	SQRT: 5
+}
+
+var input = false;
+var CurrentOp = OperationEnum.NONE;
+var numButtons = document.getElementsByClassName('numButton');
+var Led = document.getElementById('led');
+for(i = 0; i < numButtons.length; i++){
+	var t = numButtons[i];
+	t.onclick = function(){
+		if(input === false){
+			Led.innerText = this.innerText;
+			input = true;
+		}
+		else
+			Led.innerText += this.innerText;
+	}
+}
