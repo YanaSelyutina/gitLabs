e2db218f1fc77bb41823430b529b85f4a07f95ba
YanaSelyutina <selyutina1998@gmail.com>
2018-06-21 12:14:24 -1200

    123

--- js/proc.js
+++ js/proc.js
@@ -1,249 +1,238 @@
 /**
-*@var object $OperationEnum объект-перечисление всех операций
-* в калькуляторе $openum
-*/
-var OperationEnum = {
-	NONE: 0,
-	DIV: 1,
-	MUL: 2,
-	SUB: 3,
-	ADD: 4,
-	SQRT: 5,
-	SIN: 6,
-	COS: 7,
-	LOG: 8,
-	EXP: 9,
-	SQR: 10
-}
+ *@let object $operationEnum объект-перечисление всех операций
+ * в калькуляторе $openum
+ */
+let operationEnum = {
+    NONE: 0,
+    DIV: 1,
+    MUL: 2,
+    SUB: 3,
+    ADD: 4,
+    SQRT: 5,
+    SIN: 6,
+    COS: 7,
+    LOG: 8,
+    EXP: 9,
+    SQR: 10
+};
 /**
-*@var float $f первое число для вычислений
-*/
-var f = 0.;
+ *@let float $f первое число для вычислений
+ */
+let f = 0.;
 /**
-*@var float $s второе число для вычислений
-*/
-var s = 0.;
+ *@let float $s второе число для вычислений
+ */
+let s = 0.;
 /**
-*@var float $res результат вычислений
-*/
-var res = 0.;
+ *@let float $res результат вычислений
+ */
+let res = 0.;
 /**
-*@var bool $input флаг, показывающий,
-* что начался ввод следующего числа
-*/
-var input = false;
+ *@let bool $input флаг, показывающий,
+ * что начался ввод следующего числа
+ */
+let input = false;
 /**
-*@var bool $dotInput флаг, показывающий,
-* можно ли использовать точку при вводе.
-*/
-var dotInput = true;
+ *@let bool $dotInput флаг, показывающий,
+ * можно ли использовать точку при вводе.
+ */
+let dotInput = true;
 /**
-*@var int $currentOp текущая операция.
-* Задается из перечисления OperationEnum @link $openum
-*/
-var currentOp = OperationEnum.NONE;
+ *@let int $currentOp текущая операция.
+ * Задается из перечисления operationEnum @link $openum
+ */
+let currentOp = operationEnum.NONE;
 /**
-*@var object $numButtons содержит блоки кнопок с цифрами
-*/
-var numButtons = document.getElementsByClassName('numButton');
+ *@let object $numButtons содержит блоки кнопок с цифрами
+ */
+let numButtons = document.getElementsByClassName('numButton');
 /**
-*@var object $led div для записи числа
-*/
-var led = document.getElementById('led');
-
-/**
-* Устанавливает обработчик нажатия
-*/
-for(i = 0; i < numButtons.length; i++){
-	var t = numButtons[i];
-	/**
-	* Если {@link $input} == false, начинаем вводить новое число
-	*/
-	t.onclick = function(){
-	if(input === false){
-	led.innerText = this.innerText;
-	input = true;
-	dotInput = true;
-		}
-	else
-	led.innerText += this.innerText;
-	}
-}
+ *@let object $led div для записи числа
+ */
+let led = document.getElementById('led');
 
 /**
-* Выполняет общие операции для бинарных операторов
-* Если уже была установлена операция, ее выполнят
-* и начнется ожидание ввода числа.
-* Иначе нужно заполнить оба числа {@link $f} {@link $s}
-*@return none
-*/
-   function binaryOP(){
-   if(currentOp !== OperationEnum.NONE){
-    s = +led.innerText;
-	exec();
-	}
-	else{
-	f = s = +led.innerText;
-	}
-	input = false;
-}
-
-/**
-* В зависимости от текущей операции {@link $currentOp}
-* выполняется та, или иная ветка, сбрасывается ввод.
-*@return none
-*/
-   function exec(){
-   switch(currentOp){
-   case OperationEnum.SQRT:
-   res = Math.sqrt(f);
-   break;
-   case OperationEnum.SIN:
-   res = Math.sin(f);
-   break;
-   case OperationEnum.COS:
-   res = Math.cos(f);
-   break;
-   case OperationEnum.LOG:
-   res = Math.log(f);
-   break;
-   case OperationEnum.EXP:
-   res = Math.exp(f);
-   break;
-   case OperationEnum.ADD:
-   res = f+s;
-   break;
-   case OperationEnum.DIV:
-   res = f/s;
-   break;
-   case OperationEnum.MUL:
-   res = f*s;
-   break;
-  case OperationEnum.SQR:
-  res = f*f;
-  break;
-  case OperationEnum.SUB:
-  res = f - s;
-  break;
-	}
-	
-	led.innerText = res;
-	f = res;
-	currentOp = OperationEnum.NONE;
-	input = false;
-}
-/**
-* набор управляющих кнопок
-@var object $opButtons
-*/
-var sqrtButton = document.getElementById('sqrt');
-var sinButton = document.getElementById('sin');
-var cosButton = document.getElementById('cos');
-var logButton = document.getElementById('log');
-var expButton = document.getElementById('exp');
-var addButton = document.getElementById('add');
-var divButton = document.getElementById('div');
-var mulButton = document.getElementById('mul');
-var sqrButton = document.getElementById('sqr');
-var subButton = document.getElementById('sub');
-var equallyButton = document.getElementById('equally');
-var cButton = document.getElementById("c");
-var ceButton = document.getElementById("ce");
-var dotButton = document.getElementById("dot");
-
-/**
-* обработчики нажатия на управляющие кнопки.
-*@return none
-*/
-sqrtButton.onclick = function(){
-	currentOp = OperationEnum.SQRT;
-	f = +led.innerText;
-	exec();
-}
-
-sinButton.onclick = function(){
-	currentOp = OperationEnum.SIN;
-	f = +led.innerText;
-	exec();
-}
-
-cosButton.onclick = function(){
-	currentOp = OperationEnum.COS;
-	f = +led.innerText;
-	exec();
-}
-
-logButton.onclick = function(){
-	currentOp = OperationEnum.LOG;
-	f = +led.innerText;
-	exec();
-}
-
-expButton.onclick = function(){
-	currentOp = OperationEnum.EXP;
-	f = +led.innerText;
-	exec();
-}
-
-addButton.onclick = function(){
-	binaryOP();
-	currentOp = OperationEnum.ADD;
-}
-
-divButton.onclick = function(){
-	binaryOP();
-	currentOp = OperationEnum.DIV;
-}
-
-mulButton.onclick = function(){
-	binaryOP();
-	currentOp = OperationEnum.MUL;
-}
-
-sqrButton.onclick = function(){
-	currentOp = OperationEnum.SQR;
-	f = +led.innerText;
-	exec();
-}
-
-subButton.onclick = function(){
-	
-currentOp = OperationEnum.SUB;
-}
-
-equallyButton.onclick = function(){
-	if(currentOp == OperationEnum.NONE) 
-        return;
-	s = + led.innerText;
-	exec();
-}
-
-CButton.onclick = function(){
-	led.innerText = 0;
-	input = false;
-}
-
-CEButton.onclick = function(){
-	led.innerText = 0;
-	input = false;
-	currentOp = OperationEnum.NONE;
-	f = s = 0.;
-}
-
-/**
-* Кнопка точки похожа на цифровую кнопку
-* но она должна вводиться один раз
-* во время ввода числа
-*@return none
-*/
-dotButton.onclick = function(){
-	if(!dotInput) 
-        return;
-	if(input === false){
-	    led.innerText = "0.";
-	    input = true;
-        dotInput = true;
-	}else{
-        led.innerText += ".";
-	    dotInput = false;
+ * Устанавливает обработчик нажатия
+ */
+for (i = 0; i < numButtons.length; i++) {
+    let t = numButtons[i];
+    /**
+     * Если {@link $input} == false, начинаем вводить новое число
+     */
+    t.onclick = function () {
+        if (input === false) {
+            led.innerText = this.innerText;
+            input = true;
+            dotInput = true;
+        }
+        else
+            led.innerText += this.innerText;
     }
+}
+
+/**
+ * Выполняет общие операции для бинарных операторов
+ * Если уже была установлена операция, ее выполнят
+ * и начнется ожидание ввода числа.
+ * Иначе нужно заполнить оба числа {@link $f} {@link $s}
+ *@return none
+ */
+function binaryOP() {
+    if (currentOp !== operationEnum.NONE) {
+        s = +led.innerText;
+        exec();
+    }
+    else {
+        f = s = +led.innerText;
+    }
+    input = false;
+}
+
+/**
+ * В зависимости от текущей операции {@link $currentOp}
+ * выполняется та, или иная ветка, сбрасывается ввод.
+ *@return none
+ */
+function exec() {
+    switch (currentOp) {
+        case operationEnum.SQRT:
+            res = Math.sqrt(f);
+            break;
+        case operationEnum.SIN:
+            res = Math.sin(f);
+            break;
+        case operationEnum.COS:
+            res = Math.cos(f);
+            break;
+        case operationEnum.LOG:
+            res = Math.log(f);
+            break;
+        case operationEnum.EXP:
+            res = Math.exp(f);
+            break;
+        case operationEnum.ADD:
+            res = f + s;
+            break;
+        case operationEnum.DIV:
+            res = f / s;
+            break;
+        case operationEnum.MUL:
+            res = f * s;
+            break;
+        case operationEnum.SQR:
+            res = f * f;
+            break;
+        case operationEnum.SUB:
+            res = f - s;
+            break;
+    }
+    led.innerText = res;
+    f = res;
+    currentOp = operationEnum.NONE;
+    input = false;
+}
+
+/**
+ * набор управляющих кнопок
+ @let object $opButtons
+ */
+let sqrtButton = document.getElementById('sqrt');
+let sinButton = document.getElementById('sin');
+let cosButton = document.getElementById('cos');
+let logButton = document.getElementById('log');
+let expButton = document.getElementById('exp');
+let addButton = document.getElementById('add');
+let divButton = document.getElementById('div');
+let mulButton = document.getElementById('mul');
+let sqrButton = document.getElementById('sqr');
+let subButton = document.getElementById('sub');
+let equallyButton = document.getElementById('equally');
+let cButton = document.getElementById("c");
+let CEButton = document.getElementById("ce");
+let dotButton = document.getElementById("dot");
+
+/**
+ * обработчики нажатия на управляющие кнопки.
+ *@return none
+ */
+sqrtButton.onclick = function () {
+    currentOp = operationEnum.SQRT;
+    f = +led.innerText;
+    exec();
+};
+
+sinButton.onclick = function () {
+    currentOp = operationEnum.SIN;
+    f = +led.innerText;
+    exec();
+};
+cosButton.onclick = function () {
+    currentOp = operationEnum.COS;
+    f = +led.innerText;
+    exec();
+};
+logButton.onclick = function () {
+    currentOp = operationEnum.LOG;
+    f = +led.innerText;
+    exec();
+};
+expButton.onclick = function () {
+    currentOp = operationEnum.EXP;
+    f = +led.innerText;
+    exec();
+};
+addButton.onclick = function () {
+    binaryOP();
+    currentOp = operationEnum.ADD;
+};
+divButton.onclick = function () {
+    binaryOP();
+    currentOp = operationEnum.DIV;
+};
+mulButton.onclick = function () {
+    binaryOP();
+    currentOp = operationEnum.MUL;
+};
+sqrButton.onclick = function () {
+    currentOp = operationEnum.SQR;
+    f = +led.innerText;
+    exec();
+};
+subButton.onclick = function () {
+
+    currentOp = operationEnum.SUB;
+};
+
+equallyButton.onclick = function () {
+    if (currentOp === operationEnum.NONE) return;
+    s = +led.innerText;
+    exec();
+};
+cButton.onclick = function () {
+    led.innerText = 0;
+    input = false;
+};
+CEButton.onclick = function () {
+    led.innerText = 0;
+    input = false;
+    currentOp = operationEnum.NONE;
+    f = s = 0.;
+};
+
+/**
+ * Кнопка точки похожа на цифровую кнопку
+ * но она должна вводиться один раз
+ * во время ввода числа
+ *@return none
+ */
+dotButton.onclick = function () {
+    if (!dotInput) return;
+    if (input === false) {
+        led.innerText = "0.";
+        input = true;
+        dotInput = true;
+    }
+    else
+        led.innerText += ".";
+    dotInput = false;
+};
